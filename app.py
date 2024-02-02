b4de7dd08ee44c76254e4bccc324f503d5d58259
usman <usman.khawar.pk@gmail.com>
2024-02-02 23:29:22 +0500

    second commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+
+freezer = Freezer(app)
+
+@app.route('/')
+def index():
+    return render_template('catalog.html')
+
+if __name__ == '__main__':
+    freezer.run(debug=True)
